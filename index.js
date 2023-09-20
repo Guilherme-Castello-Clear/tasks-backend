5d5c28f0273bf7f500d8633eb295a907fdd5950a
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-20 10:07:48 -0300

    Feat: Middleweres configured and migrations uploaded

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-    res.status(200).send("Working")
-})
+const db = require('./config/db')
+const consign = require('consign')
+
+consign().then('./config/middlewares.js').into(app)
+
+app.db = db
+
 app.listen(3002, () => {
     console.log('Executing')
 })
